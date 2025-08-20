ed81a44ab98cd75e27886c43a311147421e2530d
unic <yuniqueunic@gmail.com>
2025-08-20 23:36:11 +0800

    refactor(app): add enabledTestItems to dependency list
    - Add enabledTestItems to the list of dependencies in the ClashRuleTester component

--- app/page.tsx
+++ app/page.tsx
@@ -335,6 +335,7 @@ export default function ClashRuleTester() {
     testGeoIP,
     testNetwork,
     testUID,
+    enabledTestItems, // Add enabledTestItems to dependencies
     ruleEngine,
     toast,
   ]);
